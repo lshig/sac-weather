47fdfe7e378ac8e315c656dfe71c4a1abbe7b19a
Liz Shigetoshi <lizshigetoshi@gmail.com>
2023-09-19 14:21:42 -0700

    :art: Create ColorThemeButton component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import './globals.css';
+import classNames from 'classnames';
 import { Inter } from 'next/font/google';
 import { useState } from 'react';
-import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
-import Navigation from './components/navigation';
 import { ThemeContext } from './context';
+import ColorThemeButton from './components/color-theme-button';
+import Navigation from './components/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,36 +16,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const [isDarkMode, setTheme] = useState(true);
-  const themeColor = isDarkMode ? 'dark' : '';
 
   return (
     <ThemeContext.Provider value={isDarkMode}>
-      <html lang="en" className={themeColor}>
+      <html lang="en" className={classNames('', { dark: isDarkMode })}>
         <body
           className={`${inter.className} bg-gray-200 text-gray-950 dark:bg-gray-950 dark:text-gray-200`}
         >
           <Navigation>
             <div className="static">
-              <button
-                type="button"
-                aria-label="Use Dark Mode"
-                className="font-semibold uppercase visible dark:invisible bg-gray-950 hover:bg-gray-700 shadow-lg rounded-full p-2 text-gray-200 absolute right-4"
-                onClick={() => {
-                  setTheme(true);
-                }}
-              >
-                <BsFillMoonStarsFill />
-              </button>
-              <button
-                type="button"
-                aria-label="Use Light Mode"
-                className="font-semibold uppercase invisible dark:visible bg-gray-700 hover:bg-gray-400 shadow-lg rounded-full p-2 text-gray-200 absolute right-4"
-                onClick={() => {
-                  setTheme(false);
-                }}
-              >
-                <BsFillSunFill />
-              </button>
+              <ColorThemeButton
+                isDarkMode={true}
+                handleToggleColorTheme={() => setTheme(true)}
+              />
+              <ColorThemeButton
+                isDarkMode={false}
+                handleToggleColorTheme={() => setTheme(false)}
+              />
             </div>
           </Navigation>
           {children}
